8ee027ff6de4338ea9b5feec76f6c863d8e7a5f0
onkarsinghsahil <onkarsinghsahil@gmail.com>
2022-09-10 16:18:05 +0000

    mido: overlay-aosp: rename lineage to aosp

--- device.mk
+++ device.mk
@@ -26,7 +26,7 @@ PRODUCT_COMPRESSED_APEX := false
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-aosp
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
