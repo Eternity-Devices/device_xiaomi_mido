79da08d9de452c471db497b983049adaa9e14fe3
onkarsinghsahil <onkarsinghsahil@gmail.com>
2022-11-14 18:03:11 +0100

    Revert "mido: init: Remove vendor.ril-daemon commands"
    This reverts commit 038f4ce19dae9aa762a83a5c21cd225e91bcfd7f.

--- rootdir/etc/init.class_main.sh
+++ rootdir/etc/init.class_main.sh
@@ -38,6 +38,7 @@ low_ram=`getprop ro.config.low_ram`
 case "$baseband" in
     "apq" | "sda" | "qcs" )
     setprop ro.vendor.radio.noril yes
+    stop vendor.ril-daemon
     stop vendor.qcrild
 esac
 
